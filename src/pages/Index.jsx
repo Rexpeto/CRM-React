e51288fdd53c039d6d17d14c37581fb8b9af2307
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-07 12:30:45 -0400

    Tabla de clientes y componente de cliente

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,13 +1,74 @@
-export const loader = () => {
+import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
-    return "desde loader";
+export const loader = () => {
+    const clientes = [
+        {
+            id: 1,
+            nombre: 'Juan',
+            telefono: 102013313,
+            email: "juan@juan.com",
+            empresa: 'Codigo Con Juan'
+        },
+        {
+            id: 2,
+            nombre: 'Karen',
+            telefono: 138198313,
+            email: "karen@juan.com",
+            empresa: 'Codigo Con Juan'
+        },
+        {
+            id: 3,
+            nombre: 'Josue',
+            telefono: 31983913,
+            email: "josue@juan.com",
+            empresa: 'Codigo Con Juan'
+        },
+        {
+            id: 4,
+            nombre: 'Miguel',
+            telefono: 319381983,
+            email: "miguel@juan.com",
+            empresa: 'Codigo Con Juan'
+        },
+        {
+            id: 5,
+            nombre: 'Pedro',
+            telefono: 1398198938,
+            email: "pedro@juan.com",
+            empresa: 'Codigo Con Juan'
+        },
+    ];
+
+    return clientes;
 }
 
 const Index = () => {
+    const clientes = useLoaderData();
+
     return ( 
         <>
             <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
             <p className="mt-3">Administra tus clientes</p>
+
+            {clientes.length ? (
+                <table className="w-full bg-white shadow mt-5 table-auto">
+                    <thead className="bg-blue-500 text-white">
+                        <tr>
+                            <th className="p-2">Cliente</th>
+                            <th className="p-2">Contacto</th>
+                            <th className="p-2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientes.map(cliente => (
+                            <Cliente key={cliente.id} cliente={cliente}/>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p>No hay clientes</p>
+            )}
         </>
     );
 }
