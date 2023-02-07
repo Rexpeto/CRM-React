b3889d51c158802cb43ff4e28cc69e0151ff1cca
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-07 11:30:09 -0400

    Actualizado

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,9 @@
 const NuevoCliente = () => {
     return ( 
-        <h1>Nuevo cliente</h1>
+        <>
+            <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+            <p className="mt-3">Administra tus clientes</p>
+        </>
     );
 }
 
