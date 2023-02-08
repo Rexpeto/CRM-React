75f0e72b2a238ba6cb682e6b5ea1debd12b9f88d
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-08 13:48:45 -0400

    Muestra varios mensaje de errores

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,13 +2,12 @@ import { useRouteError } from "react-router-dom";
 
 const Error = () => {
     const error = useRouteError();
-    console.log(error);
 
     return ( 
         <div className="space-y-8">
             <h1 className="text-center text-6xl font-semibold mt-20 text-blue-900">CRM - Clientes</h1>
             <p className="text-center">Hubo un error</p>
-            <p className="text-center">{error.message}</p>
+            <p className="text-center">{error?.statusText || error?.message || error?.axiosError?.message}</p>
         </div>
     );
 }
