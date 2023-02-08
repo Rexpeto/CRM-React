838f1496c1bc4811b0a7d672b56a267d0003d7d8
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-08 09:43:57 -0400

    Agregar clientes

--- src/data/clientes.jsx
+++ src/data/clientes.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const obtenerClientes = async () => {
     try {
         const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/clientes`);
@@ -6,5 +8,21 @@ export const obtenerClientes = async () => {
     } catch (error) {
         console.log(error);
     }
+}
 
+export const agregarClientes = async datos => {
+    try {
+        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/clientes`, {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Types': 'application/json'
+            }
+        });
+
+        await respuesta.json();
+
+    } catch (error) {
+        console.log(error)
+    }
 }
