fba88cab1d70132be1032193a2c1ec3d3abdc45f
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-08 08:29:42 -0400

    trycatch

--- src/data/clientes.jsx
+++ src/data/clientes.jsx
@@ -1,6 +1,10 @@
 export const obtenerClientes = async () => {
-    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/clientes`);
-    const resultado = await respuesta.json();
+    try {
+        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/clientes`);
+        const resultado = await respuesta.json();
+        return resultado;
+    } catch (error) {
+        console.log(error);
+    }
 
-    return resultado;
 }
