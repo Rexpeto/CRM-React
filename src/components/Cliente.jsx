c6de4c53c536d1a4c4e63eebcfdb0fb69f2b4064
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-08 13:53:43 -0400

    Botón editar

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,5 +1,9 @@
+import { useNavigate } from "react-router";
+
 const Cliente = ({cliente}) => {
     const {nombre, email, telefono, empresa, id} = cliente;
+    const navigate = useNavigate();
+
     return (
         <tr>
             <td className="p-6 space-y-2">
@@ -11,8 +15,16 @@ const Cliente = ({cliente}) => {
                 <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}</p>
             </td>
             <td className="flex justify-center gap-4 p-6">
-                <button type="button" className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs transition-colors duration-150">Editar</button>
-                <button type="button" className="text-red-600 hover:text-red-700 uppercase font-bold text-xs transition-colors duration-150">Eliminar</button>
+                <button 
+                    type="button" 
+                    className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs transition-colors duration-150"
+                    onClick={() => navigate(`/clientes/${id}/editar`)}
+                >Editar</button>
+
+                <button 
+                    type="button" 
+                    className="text-red-600 hover:text-red-700 uppercase font-bold text-xs transition-colors duration-150"
+                >Eliminar</button>
             </td>
         </tr>
     );
